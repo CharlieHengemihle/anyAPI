4ac15b828688ac3a60021cb3da293a26e42cd8d4
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-11-03 14:23:45 -0600

    first test passing with controller and model

--- sql/setup.sql
+++ sql/setup.sql
@@ -7,7 +7,7 @@ CREATE TABLE dwarves (
   name VARCHAR NOT NULL,
   purpose VARCHAR NOT NULL,
   source VARCHAR NOT NULL,
-  url VARCHAR NOT NULL,
+  url VARCHAR NOT NULL
 );
 
 INSERT INTO dwarves (name, purpose, source, url)
@@ -16,9 +16,9 @@ VALUES
 ('Balin', 'Lord of Moria', 'Middle Earth', 'https://hero.fandom.com/wiki/Balin'),
 ('Scout', 'Place flares and reach high places', 'Deep Rock Galactic', 'https://deeprockgalactic.fandom.com/wiki/Dwarves'),
 ('Driller', 'Use titanium drills and explosives to clear the way', 'Deep Rock Galactic', 'https://deeprockgalactic.fandom.com/wiki/Dwarves'),
-('Yagrum Bagran', 'Master Crafter', 'Corprusarium Bowels', 'https://elderscrolls.fandom.com/wiki/Yagrum_Bagarn_(Morrowind)'),
+('Yagrum Bagran', 'Master Crafter', 'Corprusarium Bowels', 'https://elderscrolls.fandom.com/wiki/Yagrum_Bagarn_(Morrowind)');
 
-DROP TABLE IF EXISTS elves;
+-- DROP TABLE IF EXISTS elves;
 
 -- CREATE TABLE elves (
 --   id BIGINT GENERATED ALWAYS AS IDENTITY,
